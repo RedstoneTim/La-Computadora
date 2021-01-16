ffdf11e779adaa10cbdae9f3d0761217789fafad
RedstoneTim <39703953+RedstoneTim@users.noreply.github.com>
2021-01-16 14:38:27 +0100

    Added Logger type to variable

--- src/main/kotlin/redstonetim/lacomputadora/Main.kt
+++ src/main/kotlin/redstonetim/lacomputadora/Main.kt
@@ -1,12 +1,13 @@
 package redstonetim.lacomputadora
 
 import net.dv8tion.jda.api.JDABuilder
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object Main {
     const val version = "1.0.0"
     const val prefix = "¿"
-    val logger = LoggerFactory.getLogger(Main.javaClass)
+    val logger: Logger = LoggerFactory.getLogger(Main.javaClass)
 
     @JvmStatic
     fun main(args: Array<String>) {
